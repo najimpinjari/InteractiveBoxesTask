13a6db30acfa78b0e7e1e9bec5827dc01d14d685
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2024-11-30 01:29:43 +0530

    Add project files.

---
+++ InteractiveBoxesTask/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("InteractiveBoxesTask")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("InteractiveBoxesTask")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a74ac1b2-59c5-4522-bf92-b08b1a30c844")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ InteractiveBoxesTask/assets/js/script.js
@@ -0,0 +1,23 @@
+﻿// Select all boxes
+const boxes = document.querySelectorAll('.box');
+
+boxes.forEach(box => {
+    const dot = box.querySelector('.dot');
+    const options = box.querySelector('.options');
+
+    box.addEventListener('mouseenter', () => {
+        // Change the dot color to dark when the box is selected
+        dot.style.backgroundColor = '#004d40';
+
+        // Display the options when hovering over the box
+        options.style.display = 'flex';
+    });
+
+    box.addEventListener('mouseleave', () => {
+        // Revert the dot color when the mouse leaves the box
+        dot.style.backgroundColor = '#b0bec5';
+
+        // Hide the options when the mouse leaves
+        options.style.display = 'none';
+    });
+});
